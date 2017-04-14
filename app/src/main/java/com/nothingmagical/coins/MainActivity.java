efb0501d6760db32982608e647697ac2a4356de0
Nagy Nikolett <nagyniki.hu@gmail.com>
2017-04-14 23:26:42 +0200

    Function parameter renamed from _

--- app/src/main/java/com/nothingmagical/coins/MainActivity.java
+++ app/src/main/java/com/nothingmagical/coins/MainActivity.java
@@ -204,7 +204,7 @@ public class MainActivity extends Activity {
         }
 
         @Override
-        protected void onPostExecute(Object _) {
+        protected void onPostExecute(Object result) {
             mUpdating = false;
             updateInterface();
         }
